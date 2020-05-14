7704886d65051e22e853c60e75e14aaeb0e6fc83
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-14 14:56:41 +0200

    added source code link

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // Timer >
 const d = new Date();
-const weekday = new Array(7);
-weekday[0] = "Sun";
-weekday[1] = "Mon";
-weekday[2] = "Tue";
-weekday[3] = "Wed";
-weekday[4] = "Thu";
-weekday[5] = "Fri";
-weekday[6] = "Sat";
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const day = weekday[d.getDay()];
 setInterval(() => {
   const d = new Date();
